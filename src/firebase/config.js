3527c6f2a8ac347ce07a43120434b2bc75b4721f
Rich Hildred <rhildred@gmail.com>
2021-03-21 22:09:08 -0400

    initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+import '@firebase/auth';
+import '@firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCO9V0YqpMrmh5abbGHFCqjlo1j8LtOKwI",
+    authDomain: "ux308winter2021.firebaseapp.com",
+    databaseURL: "https://ux308winter2021-default-rtdb.firebaseio.com",
+    projectId: "ux308winter2021",
+    storageBucket: "ux308winter2021.appspot.com",
+    messagingSenderId: "877645805590",
+    appId: "1:877645805590:web:1b5527cc55bcc711539d37"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+} 
+export { firebase };
